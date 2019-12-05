e5aef78f033f1de91b09172ede6185990a61fc52
ashp1731 <ashp1731@gmail.com>
2019-12-05 11:30:19 -0500

    Added class Piece

---
+++ src/CheckersCLI/Piece.java
@@ -0,0 +1,17 @@
+package CheckersCLI;
+
+public class Piece {
+
+	private String color;
+	private String rank;
+	
+	public String getColor() {return color;}
+	public void setColor(String color) {this.color = color;}
+	
+	public String getRank() {return rank;}
+	public void setRank(String rank) {this.rank = rank;}
+	
+	public void kingPiece() {
+		
+	}
+}
