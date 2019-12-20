6ae405139b895fed98c5c608d29270ffef99423f
ashwini pradhan <ashp1731@gmail.com>
2019-12-20 17:17:30 -0500

    move piece logic added

--- src/CheckersCLI/Board.java
+++ src/CheckersCLI/Board.java
@@ -2,6 +2,10 @@ package CheckersCLI;
 
 import java.util.Arrays;
 
+import Checkers.Move;
+import Checkers.Piece;
+import Checkers.Square;
+
 class Board {
 	
 	public static final int SIZE = 8;
@@ -17,45 +21,93 @@ class Board {
 	}
 	
 	
-//	public Board() {
+	public Board() {
 //		
-//		// Board Initialization
-//		Piece r = new Piece("r", "pawn");
-//		Piece b = new Piece("b", "pawn");
-//		this.squares = new Square[SIZE][SIZE];
-//		
-//		int i, j;
-//		for (i = 0; i < SIZE; i++) {
-//
-//			for (i = 1; i < SIZE; i += 2) {
-//				this.squares[i][1].setPiece(r); 
-//				this.squares[i][5].setPiece(b);
-//				this.squares[i][7].setPiece(b);
-//				this.squares[i][0].setPiece(r);
-//				this.squares[i][2].setPiece(r);
-//				this.squares[i][6].setPiece(b);
-//			}
-//		}
-//	}
+//		// Code to initialize the Board
+	squares = new Square[8][8];
 
-//	public void movePiece(Move move) {
-//		// Code added by Ashwini
-//		//Date : 12/10/2019
-//		// this method will update the array squares with final coordinates
-//
-//		
-//		
-//		if(isMoveLegal(move)) {
-//			
-//			Square sInitial = new Square();
-//			sInitial.setPiece(squares[move.getInitialXCoor()][move.getInitialYCoor()].getPiece());
-//			
-//		}
-//	}
-//
-//	public  boolean isMoveLegal(Move move){
-//		return true;
-//	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			squares[i][j] = new Square();
+			squares[i][j].setPiece(new Piece());
+		}
+	}
+	
+	squares[0][1].setPiece(new Piece("r", "pawn"));
+	squares[0][3].setPiece(new Piece("r", "pawn"));
+	squares[0][5].setPiece(new Piece("r", "pawn"));
+	squares[0][7].setPiece(new Piece("r", "pawn"));
+	squares[1][0].setPiece(new Piece("r", "pawn"));
+	squares[1][2].setPiece(new Piece("r", "pawn"));
+	squares[1][4].setPiece(new Piece("r", "pawn"));
+	squares[1][6].setPiece(new Piece("r", "pawn"));
+	squares[2][1].setPiece(new Piece("r", "pawn"));
+	squares[2][3].setPiece(new Piece("r", "pawn"));
+	squares[2][5].setPiece(new Piece("r", "pawn"));
+	squares[2][7].setPiece(new Piece("r", "pawn"));
+
+	squares[5][0].setPiece(new Piece("b", "pawn"));
+	squares[5][2].setPiece(new Piece("b", "pawn"));
+	squares[5][4].setPiece(new Piece("b", "pawn"));
+	squares[5][6].setPiece(new Piece("b", "pawn"));
+	squares[6][1].setPiece(new Piece("b", "pawn"));
+	squares[6][3].setPiece(new Piece("b", "pawn"));
+	squares[6][5].setPiece(new Piece("b", "pawn"));
+	squares[6][7].setPiece(new Piece("b", "pawn"));
+	squares[7][0].setPiece(new Piece("b", "pawn"));
+	squares[7][2].setPiece(new Piece("b", "pawn"));
+	squares[7][4].setPiece(new Piece("b", "pawn"));
+	squares[7][6].setPiece(new Piece("b", "pawn"));
+	}
+
+	public Board(Square[] square) {
+		super();
+		this.squares = squares;
+	}
+	
+	public boolean isMoveLegal(Move move) {
+		
+		return true;
+	}
+	
+	public void movePiece(Move move) {
+		// Move Piece Logic
+		Piece pieceEmpty = new Piece();
+		Piece pieceKing = new Piece("", "king");
+		
+		Piece initialPiece = new Piece();
+		initialPiece = squares[move.getInitalXCoor()][move.getInitialYCoor()].getPiece();
+		squares[move.getInitalXCoor()][move.getInitialYCoor()].setPiece(pieceEmpty);
+		squares[move.getEndingXCoor()][move.getEndingYCoor()].setPiece(initialPiece);
+
+		Piece kingPiece;
+		if (squares[move.getEndingXCoor()][move.getEndingYCoor()].getPiece().getColor() == "r"
+				&& move.getEndingYCoor() == 7) {
+			
+			kingPiece = squares[move.getEndingXCoor()][move.getEndingYCoor()].getPiece();
+			kingPiece.setRank("king");
+			squares[move.getEndingXCoor()][move.getEndingYCoor()].setPiece(kingPiece);
+			
+		} else if (squares[move.getEndingXCoor()][move.getEndingYCoor()].getPiece().getColor() == "b"
+				&& move.getEndingYCoor() == 0) {
+			
+			kingPiece = squares[move.getEndingXCoor()][move.getEndingYCoor()].getPiece();
+			kingPiece.setRank("king");
+			squares[move.getEndingXCoor()][move.getEndingYCoor()].setPiece(kingPiece);
+			
+		}
+		
+		// Jump Logic
+		
+	}
+	
+	public Move[] getLegalMoves(Player player) {
+		// Return an array containing all the legal CheckersMoves
+        // for the specfied player on the current board.  If the player
+        // has no legal moves, null is returned.
+		
+	}
+
 	
 	@Override
 	public String toString() {
